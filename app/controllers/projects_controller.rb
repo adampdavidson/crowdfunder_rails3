47756cbf40dcc3d7150812761366e2b0efacc375
adampdavidson <adam.davidson94@gmail.com>
2013-10-01 12:02:14 -0400

    making tests for show page pass

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,9 +4,9 @@ class ProjectsController < ApplicationController
     	@projects = Project.all
   	end
 
-	# def show
- #        @project = Project.find(params[:id])
- #  	end
+	def show
+        @project = Project.find(params[:id])
+  	end
 
   	def project_params
     	params.require(:title).permit(:teaser, :title, :description, :goal, :user_id)
