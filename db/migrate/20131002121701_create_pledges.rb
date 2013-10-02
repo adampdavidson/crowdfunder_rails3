0612be68464c2b399584ffc0e57c67a574bd994e
adampdavidson <adam.davidson94@gmail.com>
2013-10-02 08:19:18 -0400

    creating pledges modeland relevant associations

---
+++ db/migrate/20131002121701_create_pledges.rb
@@ -0,0 +1,10 @@
+class CreatePledges < ActiveRecord::Migration
+  def change
+    create_table :pledges do |t|
+      t.integer :user_id
+      t.integer :amount
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131001191434) do
+ActiveRecord::Schema.define(:version => 20131002121701) do
+
+  create_table "pledges", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "amount"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "projects", :force => true do |t|
     t.string   "title"
